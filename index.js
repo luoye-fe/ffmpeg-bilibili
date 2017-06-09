0265b8e61280aa2b7e9d769e5db3e801e9414e70
luoye <luoyefe@gmail.com>
2017-06-10 05:45:02 +0800

    update

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ function sendEmail(options, cb) {
 `
 	};
 	transporter.sendMail(mailOptions, function(error, info) {
+		if (error) throw error;
 		cb();
 	});
 }
@@ -45,6 +46,7 @@ function sendErrorEmail(options, cb) {
 `
 	};
 	transporter.sendMail(mailOptions, function(error, info) {
+		if (error) throw error;
 		cb();
 	});
 }
@@ -52,31 +54,37 @@ function sendErrorEmail(options, cb) {
 function run() {
 	rd.readFileFilter(config.root, /\.(mp4|avi|wmv|mpeg|ogg|rmvb)$/i, (err, files) => {
 		allFile = files;
+		allFile.sort(function(){ return 0.5 - Math.random() });
 
 		function poll() {
 			let file = allFile[0];
 			if (!file) return run();
+			console.log('开始播放： ' + file);
 			FFInstance
 				.input(file)
 				.outputOptions('-vcodec copy')
 				.outputOptions('-acodec copy')
 				.format('flv')
 				.save(config.rtmp)
+				.on('start', () => {
+					ffmpeg.ffprobe(file, function(err, data) {
+					   	sendEmail({
+					   		file: file,
+					   		detail: JSON.stringify(data, null, 4)
+					   	})
+					});
+				})
 				.on('error', function(e) {
 					sendErrorEmail({
 						file: file,
 						detail: e
 					})
+					throw e;
 				})
 				.on('end', function() {
+					console.log('播放结束：' + file);
 					allFile.splice(0, 1);
 					poll();
-				})
-				.ffprobe(function(err, data) {
-					sendEmail({
-						file: file,
-						detail: JSON.stringify(data, null, 4)
-					})
 				})
 		}
 		poll();
