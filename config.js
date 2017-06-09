6eebd7ba89ec3a39d17b785d076a40a1947c2596
luoye <luoyefe@gmail.com>
2017-06-09 22:37:23 +0800

    del

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	root: '/Users/loye/Documents/family',
-	user: 'luoye@czfcme.com',
-	pass: 'qztywzsildsgbbgc',
-	rtmp: 'rtmp://txy.live-send.acg.tv/live-txy/?streamname=live_14046215_6700054&key=effc2f8515ec9cafa81c5eea1b713b25'
-}
