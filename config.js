ac41663db4dc87100ce17290e22f5be23b31f505
luoye <luoyefe@gmail.com>
2017-06-09 22:33:39 +0800

    init

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	root: '/Users/loye/Documents/family',
+	user: 'luoye@czfcme.com',
+	pass: 'qztywzsildsgbbgc',
+	rtmp: 'rtmp://txy.live-send.acg.tv/live-txy/?streamname=live_14046215_6700054&key=effc2f8515ec9cafa81c5eea1b713b25'
+}
---
+++ index.js
@@ -0,0 +1,86 @@
+const rd = require('rd');
+const FfmpegCommand = require('fluent-ffmpeg');
+
+const config = require('./config.js');
+
+const FFInstance = new FfmpegCommand();
+
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+	host: 'smtp.qq.com',
+	secureConnection: true,
+	port: 465,
+	auth: {
+		user: config.user,
+		pass: config.pass
+	}
+});
+
+let allFile = [];
+
+function sendEmail(options, cb) {
+	const mailOptions = {
+		from: '842891024@qq.com',
+		to: 'luoyefe@gmail.com',
+		subject: 'FFMPEG-RMTP',
+		html: `
+<span>当前播放文件：</span><code>${options.file}</code><br/>
+<pre><code>${options.detail}</code></pre>
+`
+	};
+	transporter.sendMail(mailOptions, function(error, info) {
+		cb();
+	});
+}
+
+function sendErrorEmail(options, cb) {
+	const mailOptions = {
+		from: '842891024@qq.com',
+		to: 'luoyefe@gmail.com',
+		subject: 'FFMPEG-RMTP-ERROR',
+		html: `
+<span>当前播放出错文件：</span><code>${options.file}</code><br/>
+<pre><code>${options.detail}</code></pre>
+`
+	};
+	transporter.sendMail(mailOptions, function(error, info) {
+		cb();
+	});
+}
+
+function run() {
+	rd.readFileFilter(config.root, /\.(mp4|avi|wmv|mpeg|ogg|rmvb)$/i, (err, files) => {
+		allFile = files;
+
+		function poll() {
+			let file = allFile[0];
+			if (!file) return run();
+			FFInstance
+				.input(file)
+				.outputOptions('-vcodec copy')
+				.outputOptions('-acodec copy')
+				.format('flv')
+				.save(config.rtmp)
+				.on('error', function(e) {
+					sendErrorEmail({
+						file: file,
+						detail: e
+					})
+				})
+				.on('end', function() {
+					allFile.splice(0, 1);
+					poll();
+				})
+				.ffprobe(function(err, data) {
+					sendEmail({
+						file: data.format.filename,
+						detail: JSON.stringify(data, null, 4)
+					})
+				})
+		}
+		poll();
+	})
+}
+
+run();
